284198ca3aab9a441dc4f9ab021f04c36a300aa2
Precious Zuze <fadzie@macbookair.speedport.ip>
2026-01-07 17:41:50 +0100

    Add DB SSL support for hosted Postgres

--- duisburg-web-application/backend/db.js
+++ duisburg-web-application/backend/db.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const ssl =
+  process.env.DB_SSL === 'true' || process.env.PGSSLMODE === 'require'
+    ? { rejectUnauthorized: false }
+    : undefined;
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl,
 });
 
 pool.on('connect', () => {
